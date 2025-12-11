2eda42b649f80e846255f95953917646ebf6d81b
IMarcusDev <escobarmarcos511@gmail.com>
2025-12-11 02:46:05 -0500

    feat(entities/sign): refactor sign entity to accept description

--- lib/src/domain/entities/sign.dart
+++ lib/src/domain/entities/sign.dart
@@ -5,24 +5,27 @@ enum SignSection {
   FrasesComunes('Frases Comunes');
 
   final String name;
-
   const SignSection(this.name);
 }
 
 class Sign {
-  final int? id; // Opcional, por si viene de DB
+  final int? id;
   final String signTitle;
+  final String description;
   final String signVideoUrl;
   final String? signImageUrl;
   final SignSection signSection;
   final int tagId;
+  final List<String>? synonyms;
 
   Sign({
     this.id,
     required this.signTitle,
+    required this.description,
     required this.signVideoUrl,
     this.signImageUrl,
     required this.signSection,
     required this.tagId,
+    this.synonyms,
   });
 }
